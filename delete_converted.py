9cee155e953b68ccc49b3656789d73590051a495
Alexey Bogomolov <mail@abogomolov.com>
2020-10-02 20:48:24 +0300

    update logging

--- delete_converted.py
+++ delete_converted.py
@@ -10,22 +10,27 @@ logger = logging.getLogger("fileDeleter")
 logger.setLevel(logging.INFO)
 
 
-def delete_files(filepath, days=50):
+def delete_files(folder, days=50):
     """
     get files modified more that `days` ago
     delete them and log to file
     """
+    fh = logging.FileHandler(Path(folder, "log/logfile.log"), "a", "utf-8")
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(message)s", "%Y-%m-%d %H:%M:%S"
+    )
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
 
-    folder_processed = Path(filepath)
     count = 0
-    for file in folder_processed.glob("*"):
+    for file in folder.glob("*"):
         if not file.is_dir():
-            print(file)
+            # print(file)
             file_modified = datetime.fromtimestamp(file.lstat().st_mtime)
             file_date = file_modified.strftime("%H:%M %d-%b-%Y")
             if datetime.now() - file_modified > timedelta(days):
                 count += 1
-                # file.unlink()
+                file.unlink()
                 logger.info(f"deleted {file.name} created at {file_date}")
     if count == 0:
         logger.info("no old files found")
@@ -37,10 +42,4 @@ if __name__ == "__main__":
         folder = Path(folder)
         logger_folder = folder / "log"
         logger_folder.mkdir(parents=True, exist_ok=True)
-        fh = logging.FileHandler(Path(folder, "log/logfile.log"), "a", "utf-8")
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(message)s", "%Y-%m-%d %H:%M:%S"
-        )
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
         delete_files(folder)
