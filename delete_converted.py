294971f5c59bc069e22ae234f8a75c4a24eb114e
Alexey Bogomolov <mail@abogomolov.com>
2021-04-09 18:05:13 +0300

    move logger operation to prevent multiple log records

--- delete_converted.py
+++ delete_converted.py
@@ -6,26 +6,16 @@ import logging
 FOLDERS = ["//Capture2/convert/processed", "//CAPTURE2/CONVERT_fast/processed"]
 # FOLDER = "."
 
-logger = logging.getLogger("fileDeleter")
-logger.setLevel(logging.INFO)
-
 
 def delete_files(folder, days=50):
     """
     get files modified more that `days` ago
     delete them and log to file
     """
-    fh = logging.FileHandler(Path(folder, "log/logfile.log"), "a", "utf-8")
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(message)s", "%Y-%m-%d %H:%M:%S"
-    )
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
 
     count = 0
     for file in folder.glob("*"):
         if not file.is_dir():
-            # print(file)
             file_modified = datetime.fromtimestamp(file.lstat().st_mtime)
             file_date = file_modified.strftime("%H:%M %d-%b-%Y")
             if datetime.now() - file_modified > timedelta(days):
@@ -33,13 +23,23 @@ def delete_files(folder, days=50):
                 file.unlink()
                 logger.info(f"deleted {file.name} created at {file_date}")
     if count == 0:
-        logger.info("no old files found")
+        logger.info("no old files found in {}".format(folder))
     fh.close()
 
 
 if __name__ == "__main__":
+    # setup logger
+    logger_folder = Path(FOLDERS[0]) / "log"
+    logger_folder.mkdir(parents=True, exist_ok=True)
+    logger = logging.getLogger("fileDeleter")
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler(Path(logger_folder, "logfile.log"), "a", "utf-8")
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(message)s", "%Y-%m-%d %H:%M:%S"
+    )
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
     for folder in FOLDERS:
         folder = Path(folder)
-        logger_folder = folder / "log"
-        logger_folder.mkdir(parents=True, exist_ok=True)
         delete_files(folder)
